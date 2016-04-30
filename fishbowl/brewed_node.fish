1223f283f71faf2a047f60f044ed28522bcae927
Xue Can <xuecan@gmail.com>
2016-04-30 15:44:06 +0800

    改用 node-lts4 而不是 node

--- fishbowl/brewed_node.fish
+++ fishbowl/brewed_node.fish
@@ -6,15 +6,15 @@
 function reinstall-node --description='reinstall brewed node'
   set -l brewed_packages (brew list)
   if contains node $brewed_packages
-    echo_and_eval cnpm uninstall -g gulp
+    echo_and_eval cnpm uninstall -g gulp-cli
     echo_and_eval cnpm uninstall -g bower
     echo_and_eval cnpm uninstall -g node-gyp
     echo_and_eval npm uninstall -g cnpm
-    echo_and_eval brew uninstall node
+    echo_and_eval brew uninstall node4-lts
   end
-  echo_and_eval brew install node
+  echo_and_eval brew install node4-lts
   echo_and_eval npm install -g cnpm --registry=https://registry.npm.taobao.org
   echo_and_eval cnpm install -g node-gyp
   echo_and_eval cnpm install -g bower
-  echo_and_eval cnpm install -g gulp
+  echo_and_eval cnpm install -g gulp-cli
 end
