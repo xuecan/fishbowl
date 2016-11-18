0828f485346779a0241dfb266f5ccf8aed375b69
Xue Can <xuecan@gmail.com>
2016-11-18 19:27:16 +0800

    对 python 工具做了一些调整，以匹配最新的设置理念

---
+++ fishbowl/python.fish
@@ -0,0 +1,56 @@
+# Copyright (C) 2016 Xue Can <xuecan@gmail.com> and contributors.
+# Licensed under the MIT license: http://opensource.org/licenses/mit-license
+
+# 维护 ~/.pip/pip.conf 配置，使用豆瓣的 PyPI 镜像
+begin
+  if not test -e "$HOME/.pip"
+    mkdir "$HOME/.pip"
+  end
+  set -l config_file "$HOME/.pip/pip.conf"
+  echo '
+# 请不要直接编辑本文件，fishbowl 将帮助您维护
+# 如需修改，请调整 ~/.config/fish/fishbowl/python.fish 脚本
+
+[global]
+index-url = https://pypi.douban.com/simple/
+
+' > $config_file
+end
+
+# venv 命令，用于快速的进入 python virtualenv 环境
+# 如果指定的 virtualenv 不存在（不在 ~/.venv 中），则创建之
+# 如果需要使用更多的 virtualenv 参数，如使用 -p 指定另外的 python 实例，
+# 则需要自行调用 virtualenv 命令，并确保创建的 virtualenv 在 ~/.venv 目录中，
+# 后续才能使用这个命令快速进入
+function venv --description='Create or enter a Python virtual environment'
+  if not count $argv >/dev/null
+    echo 'Usage:'
+    echo '  venv <python-venv-name>'
+    return 0
+  end
+
+  if test -n "$VIRTUAL_ENV"
+    echo -n 'In virtual environment '
+    set_color green; echo -n (basename $VIRTUAL_ENV); set_color normal
+    echo -n ', please '
+    set_color green; echo -n 'deactivate'; set_color normal;
+    echo ' first.'
+    return 0
+  end
+
+  set -l name $argv[1]
+  set -l dir "$fishbowl_pyvenv_dir/$name"
+  if test -d $dir
+    source "$dir/bin/activate.fish"
+  else
+    set_color -o yellow
+    echo "Virtual environment '$name' not found, creating..."
+    set_color red
+    echo "You have about 5 seconds to press Ctrl-C to abort."
+    set_color normal
+    sleep 5
+    echo-and-eval virtualenv $dir
+    source "$dir/bin/activate.fish"
+  end
+
+end
--- profile.fish
+++ profile.fish
@@ -2,34 +2,40 @@
 # Licensed under the MIT license: http://opensource.org/licenses/mit-license
 
 
-# browser
+# 浏览器
 if test (uname) = 'Darwin'
   set -g -x BROWSER 'open'
 end
 
 
-# editor
+# 编辑器
 if test -n "$SSH_CONNECTION"
   set -g -x EDITOR 'nano'
   set -g -x VISUAL 'nano'
 else
-  set -g -x EDITOR 'atom'
-  set -g -x VISUAL 'atom'
+  set -g -x EDITOR 'code'
+  set -g -x VISUAL 'code'
 end
 
 
-# pager
+# 分页器
 set -g -x PAGER 'less'
 
 
-# language
+# 默认使用 en_US.UTF-8 能够避免很多问题
 #if test -z "$LANG"
   set -g -x LANG 'en_US.UTF-8'
 #end
 
 
-# paths
+# PATH 环境变量
 set -g -x PATH /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin $PATH
+if test -d "$HOME/Library/Python/2.7/bin"
+  set -g -x PATH $HOME/Library/Python/2.7/bin $PATH
+end
+if test -d "$HOME/Library/Python/3.5/bin"
+  set -g -x PATH "$HOME/Library/Python/3.5/bin" $PATH
+end
 if test -d "$HOME/.local/bin"
   set -g -x PATH $HOME/.local/bin $PATH
 end
@@ -40,18 +46,7 @@ set -g -x PATH (listutil rmdup $PATH)
 
 
 # extra modules
-fishbowl_load_module builtin_python2
-fishbowl_load_module brewed_python3
-fishbowl_load_module brewed_node
+fishbowl_load_module python
+fishbowl_load_module brewed-node
 fishbowl_load_module greeting
 fishbowl_load_module prompt
-
-# 临时使用的
-
-function rndc
-  command /usr/local/sbin/rndc -c /usr/local/etc/rndc.conf $argv
-end
-
-function namedcache
-  command cat /usr/local/var/named/named_dump.db
-end
