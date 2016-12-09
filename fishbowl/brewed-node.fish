c58eec465a3b0c30f84d0e6c5c07289b1d751dae
Xue Can <xuecan@gmail.com>
2016-12-10 03:37:29 +0800

    node6-lts => node@6

--- fishbowl/brewed-node.fish
+++ fishbowl/brewed-node.fish
@@ -3,17 +3,17 @@
 
 # 使用 Homebrew 安装 node.js LTS 版本，并使用淘宝提供的镜像（通过 cnpm 命令）
 # 还会在全局安装几个常用的包(可以访问 https://www.npmjs.com/ 了解这些包的用途)
-function reinstall-node --description='reinstall brewed node6-lts'
+function reinstall-node --description='reinstall brewed node@6'
     set -l brewed_packages (brew list)
-    if contains node6-lts $brewed_packages
+    if contains node@6 $brewed_packages
         echo-and-eval cnpm uninstall -g express-generator
         echo-and-eval cnpm uninstall -g gulp-cli
         echo-and-eval cnpm uninstall -g bower
         echo-and-eval cnpm uninstall -g node-gyp
         echo-and-eval npm uninstall -g cnpm
-        echo-and-eval brew uninstall node6-lts
+        echo-and-eval brew uninstall node@6
     end
-    echo-and-eval brew install node6-lts
+    echo-and-eval brew install node@6
     echo-and-eval npm install -g cnpm --registry=https://registry.npm.taobao.org
     echo-and-eval cnpm install -g node-gyp
     echo-and-eval cnpm install -g bower
